8c2f58e2a6ddc07b670ff638a7bc9606786778df
Dustin Sattler <dustin.r.sattler@gmail.com>
2022-05-27 09:56:34 -0400

    30-Days-Of-Python Day 1
    Introduction to python
    
    Learned mathematical equations
    
    Learned how to see the data types using type() function

---
+++ Day 1.py
@@ -0,0 +1,20 @@
+### Day 1 ###
+
+print(2 + 3)  # addition (+)
+print(3 - 1)  # subtraction (-)
+print(2 * 3)  # multiplication (*)
+print(4 / 2)  # division (/)
+print(3 ** 2) # exponential (**)
+print(3 % 2)  # modulus (%)
+print(3 // 2) # Floor division operator (//)
+
+# Checking data typed
+
+print(type(10))                 # Int
+print(type(3.14))               # Float
+print(type(1 + 3j))             # Complex
+print(type('String'))           # String
+print(type([1, 2, 3]))          # List
+print(type({'name': 'Dustin'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))   # Set              
+print(type((9.8, 3.14, 2.7)))   # Tuple
